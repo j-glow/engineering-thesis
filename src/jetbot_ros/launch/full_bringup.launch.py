7ef45435a2185c2fd130623c5bab008415a667b4
Jakub G <j.glowacki@samsung.com>
2023-09-01 10:33:41 +0200

    full bringup launch file almost ready

--- src/jetbot_ros/launch/full_bringup.launch.py
+++ src/jetbot_ros/launch/full_bringup.launch.py
@@ -3,69 +3,89 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration, IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.actions import DeclareLaunchArgument
-from launch_ros.actions import Node
+
 
 def generate_launch_description():
-    world = LaunchConfiguration("world")
+    ## Packages
+    jetbot_ros = get_package_share_directory("jetbot_ros")
+    gazebo_util = get_package_share_directory("gazebo_util")
 
-    world_launch_arg = DeclareLaunchArgument(
-        "world",
+    ## Launch time argument variables
+    sim_world = LaunchConfiguration("sim_world")
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    slam_params = LaunchConfiguration("slam_params")
+
+    ## Declare launch time arguments
+    declare_sim_world_cmd = DeclareLaunchArgument(
+        "sim_world",
         default_value="simple_room.world",
         description="Specify world file name",
     )
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        "use_sim_time",
+        default_value="false",
+        description="Use simulation (Gazebo) clock if true",
+    )
+    declare_slam_params_cmd = DeclareLaunchArgument(
+        "slam_params",
+        default_value="mapper_params_online_async.yaml",
+        description="Choose custom slam_toolbox params file",
+    )
 
+    ## Launches
     simulation = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             [
                 os.path.join(
-                    get_package_share_directory("gazebo_ros"),
+                    jetbot_ros,
                     "launch",
-                    "gzserver.launch.py",
+                    "simulation.launch.py",
                 )
             ]
         ),
         launch_arguments={
-            "extra_gazebo_args": "--ros-args --params-file " + gazebo_params_file,
-            "world": world,
+            "world": sim_world,
         }.items(),
     )
-
     nav = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             [
                 os.path.join(
-                    get_package_share_directory("gazebo_ros"),
+                    jetbot_ros,
                     "launch",
-                    "gzserver.launch.py",
+                    "nav.launch.py",
                 )
             ]
         ),
-        launch_arguments={
-            "extra_gazebo_args": "--ros-args --params-file " + gazebo_params_file,
-            "world": PathJoinSubstitution([gazebo_dir, "worlds", world]),
-        }.items(),
     )
-
     slam = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             [
                 os.path.join(
-                    get_package_share_directory("gazebo_ros"),
+                    get_package_share_directory("slam_toolbox"),
                     "launch",
-                    "gzserver.launch.py",
+                    "online_async_launch.py",
                 )
             ]
         ),
         launch_arguments={
-            "extra_gazebo_args": "--ros-args --params-file " + gazebo_params_file,
-            "world": PathJoinSubstitution([gazebo_dir, "worlds", world]),
+            "params_file": PathJoinSubstitution([jetbot_ros, "config", slam_params]),
+            "use_sim_time": use_sim_time,
         }.items(),
     )
 
-    ld=LaunchDescription()
-    ld.add_action(gzserver)
+    ## Declare launch descriptor and add all actions
+    ld = LaunchDescription()
+
+    ld.add_action(declare_sim_world_cmd)
+    ld.add_action(declare_use_sim_time_cmd)
+    ld.add_action(declare_slam_params_cmd)
+
+    ld.add_action(simulation)
+    # ld.add_action(nav)
+    ld.add_action(slam)
 
     return ld
