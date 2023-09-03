415c62f4f5deb8fc81236d5514ee8863e1870cc8
qbaaa-0 <ntfr123@gmail.com>
2023-09-03 21:26:05 +0200

    fix nav2 overriding twist_mux

--- src/jetbot_ros/launch/nav.launch.py
+++ src/jetbot_ros/launch/nav.launch.py
@@ -138,7 +138,8 @@ def generate_launch_description():
                 respawn_delay=2.0,
                 parameters=[configured_params],
                 arguments=['--ros-args', '--log-level', log_level],
-                remappings=remappings),
+                remappings=remappings +
+                        [('cmd_vel', 'nav_vel')]),
             Node(
                 package='nav2_bt_navigator',
                 executable='bt_navigator',
@@ -169,7 +170,7 @@ def generate_launch_description():
                 parameters=[configured_params],
                 arguments=['--ros-args', '--log-level', log_level],
                 remappings=remappings +
-                        [('cmd_vel', 'cmd_vel_nav'), ('cmd_vel_smoothed', 'cmd_vel')]),
+                        [('cmd_vel', 'cmd_vel_nav'), ('cmd_vel_smoothed', 'nav_vel')]),
             Node(
                 package='nav2_lifecycle_manager',
                 executable='lifecycle_manager',
