543e86affb097e3dfd5fd8ef8af8f8ae785a8b80
Jakub G <j.glowacki@samsung.com>
2023-09-14 12:52:24 +0200

    yolov8 test folder

---
+++ training/test_yolo.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+from ultralytics import YOLO
+import cv2
+
+
+# Person in COCO dataset is class_id==0
+
+def main():
+    model = YOLO('yolov8n.pt')
+    frame = cv2.imread('resources/people_street.jpeg')
+    result = model(frame)
+
+    frame_res = frame.copy()
+
+    for r in result:
+        frame_res = r.plot()
+
+    cv2.imshow("results",frame_res)
+
+    cv2.waitKey(1000)
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
