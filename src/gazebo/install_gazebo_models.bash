7f9813e0b613fc0a19140c902bdfc1bd679465f4
Jakub G <j.glowacki@samsung.com>
2023-09-20 16:51:57 +0200

    actor collision in world and plugin export

--- src/gazebo/install_gazebo_models.bash
+++ src/gazebo/install_gazebo_models.bash
@@ -5,5 +5,8 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 GAZEBO_MODEL_PATH=$SCRIPT_DIR/models:$GAZEBO_MODEL_PATH
 GAZEBO_PLUGIN_PATH=$SCRIPT_DIR/plugins/actor_collisions/build:$GAZEBO_PLUGIN_PATH
 
+export GAZEBO_MODEL_PATH
+export GAZEBO_PLUGIN_PATH
+
 echo Added $SCRIPT_DIR/models/ to Gazebo models search directories
 echo Added $SCRIPT_DIR/plugins/actor_collisions/build/ to Gazebo plugin search directories
