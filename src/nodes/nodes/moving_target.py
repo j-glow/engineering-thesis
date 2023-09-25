ec9750d35f2a716de20979e3a3e2dc36f9e64d9b
Jakub G <j.glowacki@samsung.com>
2023-09-25 15:31:22 +0200

    almost working generator!
    have to fix yaw calculations

--- src/nodes/nodes/moving_target.py
+++ src/nodes/nodes/moving_target.py
@@ -3,7 +3,7 @@ import math
 import message_filters
 
 from rclpy.node import Node
-from rclpy.callback_groups import ReentrantCallbackGroup
+from rclpy.callback_groups import MutuallyExclusiveCallbackGroup, ReentrantCallbackGroup
 from rclpy.executors import MultiThreadedExecutor
 from sensor_msgs.msg import LaserScan, Image, CameraInfo
 from interfaces.srv import Inference
@@ -14,10 +14,13 @@ from collections import deque
 
 class MovingTargetGenerator(Node):
     def __init__(self, buf_len):
-        super().__init__("goal_frame_gen")
+        super().__init__("moving_target")
 
         # Different callback group for service not to deadlock while nesting callbacks
-        cb_group = ReentrantCallbackGroup()
+        client_cb_group = MutuallyExclusiveCallbackGroup()
+        self.detector_client = self.create_client(
+            Inference, "image_inference", callback_group=client_cb_group
+        )
 
         # Data subscribers
         self.lidar = message_filters.Subscriber(self, LaserScan, "scan")
@@ -29,51 +32,43 @@ class MovingTargetGenerator(Node):
         self.ats = message_filters.ApproximateTimeSynchronizer(
             [self.lidar, self.camera, self.camera_info], 1, 0.1
         )
-        self.ats.registerCallback(self.process_data_cb)
-        cb_group.add_entity(self.ats)
+        self.ats.registerCallback(self._process_data_cb)
+
+        # Attributes
+        self.frame_buf = deque(maxlen=buf_len)
 
         # Publishers
         self.frame_newest = self.create_publisher(GoalFrame, "goal_frame", 5)
 
-        # Attributes
-        self.frame_buf = deque(maxlen=buf_len)
-        self.inference_request = Inference.Request()
-
-        # Data processors
-        self.detect_client = self.create_client(
-            Inference, "image_inference", callback_group=cb_group
-        )
-
-    def process_data_cb(self, scan, image, camera_info):
-        self.inference_request.image = image
-
-        while not self.detect_client.wait_for_service(timeout_sec=1.0):
+        while not self.detector_client.wait_for_service(timeout_sec=1.0):
             self.get_logger().info("Detector not initialized, waiting...")
 
+    def _process_data_cb(self, scan, image, camera_info):
+        request = Inference.Request()
+        request.image = image
         self.get_logger().info("Sending video frame.")
-        future = self.detect_client.call_async(self.inference_request)
-        self.get_logger().info("Waiting for inference results.")
-        rclpy.spin_until_future_complete(self, future, timeout_sec=5)
+        result = self.detector_client.call(request)
+        self.get_logger().info("Received inference result.")
 
-        boxes = future.result().boxes
-        delay = future.result().delay
+        self.get_logger().info(f"Current inference delay: {result.delay:.2f}ms")
 
-        self.get_logger().info(f"Current inference delay: {delay:.2f}ms")
-        if len(boxes) == 0:
+        if len(result.boxes) == 0:
             self.get_logger().info("No matches in current frame.")
             return
 
-        if len(boxes) > 1:
+        if len(result.boxes) > 1:
             self.get_logger().warn("More than one person detected.")
             self.get_logger().warn(
                 "Currently multiple person detection is unsupported."
             )
             self.get_logger().warn("Choosing first occurence for processing.")
 
-        bbox_center = (boxes[0][0] + boxes[0][2]) / 2
-        frame_center = camera_info.K[2]
+        self.get_logger().info(f"{result.boxes}")
+        bbox = result.boxes[0].xyxyn
+        bbox_center = (bbox[0] + bbox[2]) / 2
+        frame_center = camera_info.k[2]
         img_width = camera_info.width
-        focal_len_x = camera_info.K[0]
+        focal_len_x = camera_info.k[0]
 
         angle = math.atan((bbox_center - frame_center) * img_width / focal_len_x)
 
@@ -87,15 +82,16 @@ class MovingTargetGenerator(Node):
         self.frame_newest.publish(data)
 
 
+
 def main():
     rclpy.init()
-    frame_gen = MovingTargetGenerator(20)
+    node = MovingTargetGenerator(20)
     executor = MultiThreadedExecutor()
-    executor.add_node(frame_gen)
+    executor.add_node(node)
 
     executor.spin()
 
-    frame_gen.destroy_node()
+    node.destroy_node()
     rclpy.shutdown()
 
 
